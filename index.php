9ee31a0e58262e3876394f34cf6778537bbc133e
Guiziin0909 <132912110+Guiziin0909@users.noreply.github.com>
2023-05-10 12:05:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,8 @@
     Gosto também de assistir filmes, jogar bola, fazer musculação e jogar vídeo-game.</p>
 
 
-date_default_timezone_set('America/Sao_Paulo');
+date_default_timezone_set
+('America/Sao_Paulo');
 
  echo date('h:i:s A');
  <?php
