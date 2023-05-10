522cd9468cd7aa8d00e538553552ba2dda932feb
Guiziin0909 <132912110+Guiziin0909@users.noreply.github.com>
2023-05-10 10:44:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-echo "Olá, turma do az900";
-?>
+<!DOCTYPE
+<html>
+     <head>
+    <title>Minha página inicial</title>
+    </head>
+<body>
+       <h1>Minha página inicial</h1>
+       <p>Bem vindo(a) à minha página inicial! Aqui você encontrará informações sobre mim e meus interesses.</p>
+
+   <p>Sou um(a) aluno(a) iniciante em programação e cloud computing
+    Gosto também de assistir filmes, jogar bola, fazer musculação e jogar vídeo-game.</p>
+
+<a href= "#" class="button">Entre em contato</a>
+    </body>
+
+
+
+</html>
